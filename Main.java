c19770977bcfe878e0bbced00c501803d3b701f5
Grzesiek1337 <gmiedzian@gmail.com>
2021-02-13 19:13:57 +0100

    New

--- Main.java
+++ Main.java
@@ -8,23 +8,20 @@ public class Main {
     public static void main(String[] args) {
 
         String klocek = "[  ]";
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Podaj ilosc poziomow piramidy: ");
-            int poziomy = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Podaj ilosc poziomow piramidy: ");
+        int poziomy = sc.nextInt();
 
-            for (int i = 0; i < poziomy; i++) {
-                for (int j = 0; j < poziomy + 3 ; j++) {
-                    if (j < (poziomy - i) || j > (poziomy + i)) {
-                        System.out.print(" ");
-                    } else {
-                        System.out.print(klocek);
-                    }
+        for (int i = 0; i < poziomy; i++) {
+            for (int j = 0; j < poziomy + 3 ; j++) {
+                if (j < (poziomy - i) || j > (poziomy + i)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(klocek);
                 }
-                System.out.println();
             }
+            System.out.println();
         }
+        System.out.println("Nie siema nie żegam się.");
     }
-
-
-
-
+}

75d8990d7f3542fe994f362b7d47af5ff4ed0ad0
Grzesiek1337 <gmiedzian@gmail.com>
2021-04-08 18:27:53 +0200

    Kolejna edycja5

--- Main.java
+++ Main.java
@@ -1,20 +1,38 @@
 package com.grzes;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        question();
 
 
     }
+
+    public static void question() {
+        Scanner scanner = new Scanner(System.in);
+        int hitCounts = 0;
+
+        boolean capitol = false;
+        while (!capitol) {
+            System.out.println("Jakie miasto to obecnie stolica Polski?");
+            String answer = scanner.nextLine();
+            if (answer.equals("Warszawa")) {
+                System.out.println("Brawo,to jest poprawna odpowiedź!");
+                System.out.println("Udało Ci się za "+hitCounts+" razem!");
+                capitol = true;
+            } else  {
+                System.out.println("Nieprawda! Spróbuj znowu.");
+                hitCounts++;
+
+
+            }
+
+        }
+
+    }
+
+
 }
-
-
-
-
-
-
-
-
-
-
